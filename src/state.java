74ff24b45ec86409104db397c7bc088640c8ed59
alaaalzoubi1 <alaaalzoubu321@gmail.com>
2023-10-23 15:42:27 +0300

    hello

---
+++ src/state.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class state {
+    public int column;
+    public int row;
+
+
+    public state(int row, int column)
+    {
+        this.column = column;
+        this.row = row;
+    }
+}
